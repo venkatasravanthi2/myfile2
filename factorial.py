a3a0fc41cad50f7628fae8d7abe6f005f2285c63
venkatasravanthi2 <41746796+venkatasravanthi2@users.noreply.github.com>
2018-08-03 16:24:35 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,10 +1,10 @@
-num=int(input())
+n=int(input())
 factorial = 1
-if num < 0:
-   print(num)
-elif num == 0:
-   print(num)
+if n< 0:
+   print(n)
+elif n == 0:
+   print(n)
 else:
-   for i in range(1,num + 1):
+   for i in range(1,n + 1):
        factorial = factorial*i
    print(factorial)
